b0d212c381a51e434362567b4c28a70c2127c729
WhoTFTookShadowFlare <76539823+WhoTFTookShadowFlare@users.noreply.github.com>
2026-06-09 12:22:11 -0600

    Basic terminal drawing utilities

---
+++ DrawFrame.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finalProject
+{
+	public sealed class DrawFrame
+	{
+		private static readonly TerminalPixel defaultPixel = new TerminalPixel(' ');
+
+		private readonly int _width;
+		private readonly int _height;
+
+		private readonly TerminalPixel[,] _pixels;
+
+		public int Width { get { return _width; } }
+		public int Height { get { return _height; } }
+
+		public DrawFrame(int width, int height)
+		{
+			_width = width;
+			_height = height;
+
+			_pixels = new TerminalPixel[width, height];
+			for(int x = 0; x < width; x++)
+			{
+				for (int y = 0; y < height; y++)
+				{
+					_pixels[x, y] = defaultPixel;
+				}
+			}
+		}
+
+		private int CalcPixelIndex(int x, int y)
+		{
+			return x + y * Width;
+		}
+
+		public DrawFrame DrawText(
+			int x, int y, string text,
+			TerminalColor fgColor = TerminalColor.WHITE, TerminalColor bgColor = TerminalColor.DONT_CARE
+			)
+		{
+			for (int idx = 0; idx < text.Length; idx++)
+			{
+				TerminalColor targetFGColor = fgColor;
+				TerminalColor targetBGColor = bgColor;
+				if (fgColor == TerminalColor.DONT_CARE) targetFGColor = _pixels[x + idx, y].ForegroundColor;
+				if (bgColor == TerminalColor.DONT_CARE) targetBGColor = _pixels[x + idx, y].BackgroundColor;
+                _pixels[x + idx, y] = new TerminalPixel(text[idx], targetFGColor, targetBGColor);
+			}
+
+			return this;
+		}
+
+		public DrawFrame DrawRect(int x, int y, int width, int height, TerminalColor color)
+		{
+			for(int rx = 0; rx < height; rx++)
+			{
+				for(int ry = 0; ry < width; ry++)
+				{
+					_pixels[x + rx, y + ry] = new TerminalPixel(' ', color, color);
+				}
+			}
+
+			return this;
+		}
+
+		public void Write(TextWriter output)
+		{
+			StringBuilder frameOutput = new StringBuilder();
+			frameOutput.Capacity = Width * Height + 4096;
+			output.Write("\x1b[H");
+			for (int x = 0; x < Height; x++)
+			{
+				for (int y = 0; y < Width; y++)
+				{
+					frameOutput.Append(_pixels[y, x].ToString());
+				}
+			}
+			output.Write(frameOutput.ToString());
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,71 @@
 ﻿namespace finalProject
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+	internal class Program
+	{
+		string[] displays =
+		{
+			"[\\]",
+			"[|]",
+			"[/]",
+			"[-]"
+		};
+
+		ConsoleKey key = ConsoleKey.LeftWindows;
+		int winWidth = 0, winHeight = 0;
+
+		public Program()
+		{	}
+
+		~Program()
+		{	}
+
+		private DrawFrame SetupDraw()
+		{
+			Console.CursorVisible = false;
+			winWidth = Console.WindowWidth;
+			winHeight = Console.WindowHeight;
+
+			DrawFrame frame = new(winWidth, winHeight);
+			return frame;
+		}
+
+		public void Run()
+		{
+			int current = 0;
+			while (key != ConsoleKey.Escape)
+			{
+				while (Console.KeyAvailable)
+				{
+					key = Console.ReadKey().Key;
+				}
+
+				DrawFrame frame = SetupDraw();
+
+				current = (current + 1) % displays.Length;
+				frame
+					.DrawRect(0, 0, 5, 5, TerminalColor.BRIGHT_RED)
+					.DrawText(3, 0, displays[current], TerminalColor.WHITE, TerminalColor.DONT_CARE)
+                    .DrawText(5, 5, displays[current]);
+
+				frame.Write(Console.Out);
+				Thread.Sleep((int)Math.Floor(1.0 / 60.0 * 1000));
+			}
+
+			CleanUp();
+		}
+
+		private static void CleanUp()
+		{
+			Console.CursorVisible = true;
+			Console.WriteLine("\x1b[0m");
+			Console.WriteLine("\x1b[H");
+			Console.WriteLine("\x1b[2J");
+		}
+
+		static void Main(string[] args)
+		{
+			Program app = new();
+			app.Run();
+		}
+	}
 }
